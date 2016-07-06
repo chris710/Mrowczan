6abbe03f955c2d6d1c7662c0c3c381ebe255b0fe
Krzesimir Samborski <krzesimi.samborski@imag.fr>
2016-07-06 22:35:49 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,22 @@
+DOMAIN = {
+    'user': {
+        'schema': {
+            'firstname': {
+                'type': 'string'
+            },
+            'lastname': {
+                'type': 'string'
+            },
+            'username': {
+                'type': 'string',
+                'unique': True
+            },
+            'password': {
+                'type': 'string'
+            },
+            'phone': {
+                'type': 'string'
+            }
+        }
+    }
+}
