763ee059de539ecb501a0256e25821a240fb24bc
Krzesimir Samborski <krzesimi.samborski@imag.fr>
2016-07-07 12:19:38 +0200

    REST part of example

--- rest.py
+++ rest.py
@@ -1,12 +1,11 @@
-from flask import Flask
+#from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#app = Flask(__name__)
 
 
-if __name__ == '__main__':  # when run as main program
-    app.run()
+#@app.route('/')
+#def hello_world():
+    #return 'Hello World!'
+
+#if __name__ == '__main__':  # when run as main program
+    #app.run()
--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+RESOURCE_METHODS = ['GET', 'POST']
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+X_DOMAINS = '*'
+X_HEADERS = ['Authorization', 'If-Match', 'Access-Control-Expose-Headers', 'Content-Type', 'Pragma', 'Cache-Control']
+X_EXPOSE_HEADERS = ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
 DOMAIN = {
     'user': {
         'schema': {
@@ -17,6 +22,22 @@ DOMAIN = {
             'phone': {
                 'type': 'string'
             }
+        },
+        'additional_lookup': {
+            'url': 'regex("[\w]+")',
+            'field': 'username',
         }
+    },
+    'item': {
+        'MONGO_QUERY_BLACKLIST' : ['$where'],
+        'schema': {
+            'name': {
+                'type': 'string'
+            },
+            'username': {
+                'type': 'string'
+            }
+        },
+        'resource_methods': ['GET', 'POST', 'DELETE'],
     }
 }
