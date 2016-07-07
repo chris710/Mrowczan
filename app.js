43693198ef6d95ab4d53149dea8e16449d2de9be
Krzesimir Samborski <krzesimi.samborski@imag.fr>
2016-07-07 12:20:44 +0200

    Flask app index and sign in stub

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by st3rn on 07/07/16.
+ */
+
+var express = require('express'), app = express();
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',function (req,res) {
+    res.sendFile('index.html',{'root':__dirname+'/public/index'});
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
