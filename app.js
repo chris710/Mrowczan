caa235f96c22a7e760d1995bab6e6c5168c323b2
Krzesimir Samborski <krzesimi.samborski@imag.fr>
2016-07-07 12:33:55 +0200

    Flask app sign in and sign up stub

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get('/',function (req,res) {
     res.sendFile('index.html',{'root':__dirname+'/public/index'});
 });
 
+app.get('/signIn', function (req,res) {
+    res.sendFile('signin.html', {
+        'root': dirname+'/public/signin'
+    });
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
