013b9d8c1e0017c669dc54d16f5ecac0c5e6c6bf
Krzesimir Samborski <krzesimi.samborski@imag.fr>
2016-07-08 20:58:05 +0200

    Updating and deleting items

--- public/userHome/userHome.js
+++ public/userHome/userHome.js
@@ -52,4 +52,59 @@ angular.module('userHome', ['ngRoute', 'myAppService'])
         $scope.edit.tag = tag;
         $('#editModal').modal('show');
     }
+
+    $scope.update = function () {
+        $http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"};
+        $http.defaults.headers.common = {"Access-Control-Expose-Headers": "Origin, X-Requested-With, Content-Type, Accept"};
+        $http.defaults.headers.common["Cache-Control"] = "no-cache";
+        $http.defaults.headers.common.Pragma = "no-cache";
+        $http.defaults.headers.common['Authorization'] = 'Basic '+auth;
+        $http.defaults.headers.common['If-Match'] = $scope.edit.tag;
+        
+        $http({
+            method: 'PATCH',
+            cache: false,
+            url: 'http://127.0.0.1:5000/item/'+$scope.edit.id,
+            data: {
+                name: $scope.edit.task
+            }
+        }).
+        success(function (data,status,headers,config) {
+            $('#editModal').modal('hide');
+            getAllTask();
+        }).
+        error(function(data,status,headers,config) {
+            console.log(data, status);
+        })
+    }
+    
+    $scope.deletion = {};
+    
+    $scope.confirmDelete = function (id, tag) {
+        $scope.deletion.id = id;
+        $scope.deletion.tag = tag;
+        $('#deleteModal').modal('show');
+    }
+    
+    $scope.deleteTask = function(){
+		$http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"};
+        $http.defaults.headers.common = {"Access-Control-Expose-Headers": "Origin, X-Requested-With, Content-Type, Accept"};
+		$http.defaults.headers.common["Cache-Control"] = "no-cache";
+        $http.defaults.headers.common.Pragma = "no-cache";
+        $http.defaults.headers.common['Authorization'] = 'Basic '+auth;
+		$http.defaults.headers.common['If-Match'] = $scope.deletion.tag;
+
+		$http({
+            method: 'DELETE',
+            cache: false, 
+            url: 'http://127.0.0.1:5000/item/'+ $scope.deletion.id}).
+		    success(function(data, status, headers, config) {
+                $('#deleteModal').modal('hide');
+                getAllTask();
+		    }).
+		    error(function(data, status, headers, config) {
+		        console.log(data,status);
+		    });	
+	}
+    
 }]);
